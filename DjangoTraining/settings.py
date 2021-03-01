24fb22fae3579d8918d01d3865d8020ded73e7eb
aaxyat <aaxyat@gmail.com>
2021-03-01 16:40:16 +0000

    Implement DJANGO_SECRET variable

--- DjangoTraining/settings.py
+++ DjangoTraining/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from os import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!tl@)2**nr(fs&co@#x7k-p^rcfgwkrzw%dhvfnsr$z&=x$w3b'
+SECRET_KEY = environ["DJANGO_SECRET"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
